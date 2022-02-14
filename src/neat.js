000f390f1a3323d5e7236877a359a1b0404131a2
Nigel Leck <nigel@stSoftware.com.au>
2022-02-14 14:18:58 +1100

    more debug messsages

--- src/neat.js
+++ src/neat.js
@@ -88,7 +88,9 @@ Neat.prototype = {
     const fittest = Network.fromJSON(this.population[0].toJSON());
     fittest.score = this.population[0].score;
     if( isFinite(fittest.score)==false){
-      console.warn( "this.population[0]",this.population[0].toJSON());
+      for( let i = 0;i < this.population.length;i++){
+        console.warn( "this.population["+i+"]",this.population[i].toJSON());
+      }      
     }
     const newPopulation = [];
 
