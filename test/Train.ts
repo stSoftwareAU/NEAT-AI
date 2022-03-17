8a8acb703fe7cb77923325998fb0be2d324eeb03
Nigel Leck <Nigel.Leck@dta.gov.au>
2022-03-17 19:46:59 +1100

    check boolean

--- test/Train.ts
+++ test/Train.ts
@@ -70,6 +70,37 @@ Deno.test("XOR", async () => {
   assert(results.error <= 0.03, "Error rate was: " + results.error);
 });
 
+Deno.test("x",()=>{
+  
+console.log( "value",  +false );
+});
+
+Deno.test("booleanXOR", async () => {
+  // Train the XOR gate
+  const trainingSet = [
+    { input: [false, false], output: [0] },
+    { input: [false, true], output: [1] },
+    { input: [true, false], output: [1] },
+    { input: [true, true], output: [0] },
+  ];
+
+  const network = new Network(2, 1);
+  const results = await network.evolve(trainingSet, {
+    mutation: Mutation.FFW,
+    equal: true,
+    elitism: 10,
+    mutationRate: 0.5,
+    error: 0.03,
+    threads: 1,
+  });
+
+  assert(results.error <= 0.03, "Error rate was: " + results.error);
+
+  const value=network.activate([1,0])[0];
+
+  assert( value > 0.7, "Should be more than 0.7 was: " + value);
+});
+
 Deno.test("XNOR", async () => {
   // Train the XNOR gate
   const trainingSet = [
