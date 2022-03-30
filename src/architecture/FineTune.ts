b0f0ecdb19bf922e3544b41be27b9d66723f05ba
Nigel Leck <monster.flying.s@gmail.com>
2022-03-30 12:21:28 +1100

    write out data

--- src/architecture/FineTune.ts
+++ src/architecture/FineTune.ts
@@ -57,9 +57,9 @@ export function fineTuneImprovement(
           );
           c.nodes[i].bias = bias;
           fineTuned.push(c);
-          Deno.writeTextFileSync( ".fittest.json", JSON.stringify( fn));
+          Deno.writeTextFileSync( ".fittest.json", JSON.stringify( fittest));
           Deno.writeTextFileSync( ".variant.json", JSON.stringify( c));
-          Deno.writeTextFileSync( ".previous.json", JSON.stringify( pn));
+          Deno.writeTextFileSync( ".previous.json", JSON.stringify( previousFittest));
         }
         break;
       }
