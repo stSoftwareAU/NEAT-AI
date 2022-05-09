a275a7b9d79f2839c2436cd59c4255799a505186
Nigel (MacBook) <nigel@laptop>
2022-05-09 11:47:29 +1000

    don't ignore case

---
+++ src/Neat.js
@@ -0,0 +1,372 @@
+/* Import */
+import { Network } from "./architecture/network.js";
+import { fineTuneImprovement } from "./architecture/FineTune.ts";
+import { Methods } from "./methods/methods.js";
+import { make as makeConfig } from "./config/NeatConfig.ts";
+import { makeElitists } from "../src/architecture/elitism.ts";
+import { addTag, getTag } from "../src/tags/TagsInterface.ts";
+import { Fitness } from "./architecture/Fitness.ts";
+import { emptyDirSync } from "https://deno.land/std@0.137.0/fs/empty_dir.ts";
+import { NeatUtil } from "./NeatUtil.ts";
+
+/* Easier variable naming */
+const selection = Methods.selection;
+
+/*******************************************************************************
+                                         NEAT
+*******************************************************************************/
+export class Neat {
+  constructor(input, output, options, workers) {
+    this.input = input; // The input size of the networks
+    this.output = output; // The output size of the networks
+
+    this.workers = workers ? workers : [];
+    this.config = makeConfig(options);
+    this.util = new NeatUtil(this, this.config);
+
+    // The fitness function to evaluate the networks
+    this.fitness = new Fitness(this.workers, this.config.growth);
+    // Generation counter
+    this.generation = 0;
+    this.trainRate = this.config.trainRate;
+
+    // Initialise the genomes
+    this.population = this.config.creatures;
+  }
+
+  /**
+   * Create the initial pool of genomes
+   */
+  async populatePopulation(network) {
+    while (this.population.length < this.config.popsize) {
+      let copy;
+      if (network) {
+        copy = Network.fromJSON(network.toJSON());
+      } else {
+        copy = new Network(this.input, this.output);
+      }
+
+      this.population.push(copy);
+    }
+
+    if (network) {
+      this.population.unshift(network);
+    }
+
+    await this.util.deDepulate(this.population);
+  }
+
+  /**
+   * Evaluates, selects, breeds and mutates population
+   */
+  async evolve(previousFittest) {
+    const trainPromises = [];
+    for (
+      let i = 0;
+      i < this.population.length && i < this.workers.length;
+      i++
+    ) {
+      const n = this.population[i];
+      if (n.score) {
+        const p = this.workers[i].train(n, this.trainRate);
+        trainPromises.push(p);
+      }
+    }
+    await this.evaluate();
+
+    /* Elitism: we need at least 2 on the first run */
+    const elitists = makeElitists(
+      this.population,
+      this.config.elitism > 1
+        ? this.config.elitism
+        : previousFittest
+        ? this.config.elitism
+        : 2,
+    );
+    const tmpFittest = elitists[0];
+
+    const fittest = Network.fromJSON(tmpFittest.toJSON()); // Make a copy so it's not mutated.
+    fittest.score = tmpFittest.score;
+    addTag(fittest, "score", fittest.score.toString());
+    addTag(fittest, "error", getTag(fittest, "error"));
+
+    const livePopulation = [];
+
+    await this.util.writeScores(
+      this.population,
+    );
+
+    let trainingWorked = false;
+    
+    for (let i = 0; i < this.population.length; i++) {
+      const p = this.population[i];
+
+      if (isFinite(p.score)) {
+        const oldScore = getTag(p, "old-score");
+        if (oldScore && p.score <= parseFloat(oldScore)) {
+          /** If fine tuning made no improvement then remove to prevent flooding of the population with clones. */
+          continue;
+        }
+
+        livePopulation.push(p);
+
+        const untrained = getTag(p, "untrained");
+
+        if (untrained) {
+          const error = getTag(p, "error");
+          const currentError = parseFloat(error) * -1;
+          const previousError = parseFloat(untrained) * -1;
+
+          if (currentError <= previousError) {
+            // console.info( "Training worked", previousError, currentError );
+            trainingWorked = true;
+          }
+        }
+      }
+    }
+
+    if (previousFittest) {
+      if (trainingWorked) {
+        const nextRate = Math.min(this.trainRate * (1 + Math.random()), 0.1);
+        
+        this.trainRate = nextRate;
+      } else {
+        const nextRate = Math.max(this.trainRate * Math.random(), 0.000_000_01);
+        this.trainRate = nextRate;
+      }
+    }
+
+    this.population = livePopulation;
+
+    /**
+     * If this is the first run then use the second best as the "previous"
+     *
+     * If the previous fittest and current fittest are the same then try another out of the list of the elitists.
+     */
+    let rebootedFineTune = false;
+    let tmpPreviousFittest = previousFittest;
+
+    if (!tmpPreviousFittest) {
+      tmpPreviousFittest = elitists[1];
+    } else if (elitists.length > 1) {
+      const previousScoreTxt = getTag(tmpPreviousFittest, "score");
+      if (previousScoreTxt) {
+        const previousScore = parseFloat(previousScoreTxt);
+        if (previousScore == fittest.score) {
+          let pos;
+
+          for (let attempts = 0; attempts < 3; attempts++) {
+            pos = Math.floor(Math.random() * elitists.length) + 1;
+            tmpPreviousFittest = elitists[pos];
+
+            if (tmpPreviousFittest) break;
+          }
+
+          if (tmpPreviousFittest) {
+            const previousScoreTxt2 = getTag(tmpPreviousFittest, "score");
+            if (!previousScoreTxt2) {
+              console.info("No score for elitist", pos);
+            } else {
+              const previousScore2 = parseFloat(previousScoreTxt2);
+              if (previousScore2 < fittest.score) {
+                console.info(
+                  "Rebooting fine tuning, elitist:",
+                  pos,
+                );
+                rebootedFineTune = true;
+              } else {
+                console.info(
+                  "FAILED: Rebooting fine tuning: previous score not less than current",
+                  pos,
+                  previousScore2,
+                  fittest.score,
+                );
+              }
+            }
+          } else {
+            console.info("FAILED Rebooting fine tuning: no creature at", pos);
+          }
+        }
+      }
+    }
+
+    const fineTunedPopulation = fineTuneImprovement(
+      fittest,
+      tmpPreviousFittest,
+      /** 20% of population or those that just died */
+      Math.max(
+        Math.ceil(this.config.popsize / 5),
+        this.config.popsize - this.population.length,
+      ),
+      !rebootedFineTune,
+    );
+
+    const newPopulation = [];
+
+    // Breed the next individuals
+    for (
+      let i = this.config.popsize - elitists.length -
+        fineTunedPopulation.length;
+      i--;
+    ) {
+      newPopulation.push(this.getOffspring());
+    }
+
+    // Replace the old population with the new population
+    this.util.mutate(newPopulation);
+
+    const trainPopulation = [];
+    let tCounter = 0;
+    emptyDirSync(".debug");
+    await Promise.all(trainPromises).then((results) => {
+      results.forEach((r) => {
+        if (r.train) {
+          if (isFinite(r.train.error)) {
+            const json = JSON.parse(r.train.network);
+
+            addTag(json, "approach", "trained");
+            addTag(json, "error", r.train.error);
+            addTag(json, "duration", r.duration);
+            Deno.writeTextFileSync(
+              ".debug/train-" + tCounter + ".json",
+              JSON.stringify(json, null, 2),
+            );
+
+            const tmpElite = elitists[tCounter];
+            if (tmpElite) {
+              Deno.writeTextFileSync(
+                ".debug/elitist-" + tCounter + ".json",
+                JSON.stringify(elitists[tCounter].toJSON(), null, 2),
+              );
+            }
+            tCounter++;
+            trainPopulation.push(Network.fromJSON(json));
+          }
+        } else {
+          throw "No train result";
+        }
+      });
+    });
+
+    this.population = [
+      ...elitists,
+      ...fineTunedPopulation,
+      ...newPopulation,
+      ...trainPopulation,
+    ]; // Keep pseudo sorted.
+
+    await this.util.deDepulate(this.population);
+    this.generation++;
+
+    return fittest;
+  }
+
+  /**
+   * Breeds two parents into an offspring, population MUST be sorted
+   */
+  getOffspring() {
+    return Network.crossOver(
+      this.getParent(),
+      this.getParent(),
+      this.config.equal,
+    );
+  }
+
+  /**
+   * Evaluates the current population
+   */
+  async evaluate() {
+    if (this.config.clear) {
+      for (let i = this.population.length; i--;) {
+        this.population[i].clear();
+      }
+    }
+
+    try {
+      await this.fitness.calculate(this.population);
+    } catch (e) {
+      console.error("fitness error", e);
+      throw e;
+    }
+  }
+  /**
+   * Gets a genome based on the selection function
+   * @return {Network} genome
+   */
+  getParent() {
+    switch (this.config.selection) {
+      case selection.POWER: {
+        if (this.population[0].score < this.population[1].score) {
+          console.trace();
+          throw "Not Sorted";
+        }
+
+        const index = Math.floor(
+          Math.pow(Math.random(), this.config.selection.power) *
+            this.population.length,
+        );
+        return this.population[index];
+      }
+      case selection.FITNESS_PROPORTIONATE: {
+        // As negative fitnesses are possible
+        // https://stackoverflow.com/questions/16186686/genetic-algorithm-handling-negative-fitness-values
+        // this is unnecessarily run for every individual, should be changed
+        let totalFitness = 0;
+        let minimalFitness = 0;
+        for (let i = this.population.length; i--;) {
+          const score = this.population[i].score;
+          minimalFitness = score < minimalFitness ? score : minimalFitness;
+          totalFitness += score;
+        }
+
+        const adjustFitness = Math.abs(minimalFitness);
+        totalFitness += adjustFitness * this.population.length;
+
+        const random = Math.random() * totalFitness;
+        let value = 0;
+
+        for (let i = 0; i < this.population.length; i++) {
+          const genome = this.population[i];
+          value += genome.score + adjustFitness;
+          if (random < value) {
+            return genome;
+          }
+        }
+
+        // if all scores equal, return random genome
+        return this
+          .population[Math.floor(Math.random() * this.population.length)];
+      }
+      case selection.TOURNAMENT: {
+        if (this.config.selection.size > this.config.popsize) {
+          throw new Error(
+            "Your tournament size should be lower than the population size, please change methods.selection.TOURNAMENT.size",
+          );
+        }
+
+        // Create a tournament
+        const individuals = new Array(this.config.selection.size);
+        for (let i = 0; i < this.config.selection.size; i++) {
+          const random =
+            this.population[Math.floor(Math.random() * this.population.length)];
+          individuals[i] = random;
+        }
+
+        // Sort the tournament individuals by score
+        individuals.sort(function (a, b) {
+          return b.score - a.score;
+        });
+
+        // Select an individual
+        for (let i = 0; i < this.config.selection.size; i++) {
+          if (
+            Math.random() < this.config.selection.probability ||
+            i === this.config.selection.size - 1
+          ) {
+            return individuals[i];
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/architecture/Node.js
@@ -0,0 +1,447 @@
+/* Import */
+import { Methods } from "../methods/methods.js";
+import { Mutation } from "../methods/mutation.ts";
+import { Connection } from "./connection.js";
+// import { Config } from "../config.ts";
+
+/*******************************************************************************
+                                         NODE
+*******************************************************************************/
+
+export function Node(type) {
+  this.bias = (type === "input") ? 0 : Math.random() * 0.2 - 0.1;
+  this.squash = Methods.activation.LOGISTIC;
+  this.type = type || "hidden";
+
+  this.activation = 0;
+  this.state = 0;
+  this.old = 0;
+
+  // For tracking momentum
+  this.previousDeltaBias = 0;
+
+  // Batch training
+  this.totalDeltaBias = 0;
+
+  this.connections = {
+    in: [],
+    out: [],
+    gated: [],
+    self: new Connection(this, this, 0),
+  };
+
+  // Data for backpropagation
+  this.error = {
+    responsibility: 0,
+    projected: 0,
+    gated: 0,
+  };
+}
+
+Node.prototype = {
+  /**
+   * Activates the node
+   */
+  activate: function (input) {
+    // Check if an input is given
+    if (typeof input !== "undefined") {
+      this.activation = input;
+      return this.activation;
+    }
+
+    this.old = this.state;
+
+    // All activation sources coming from the node itself
+    this.state =
+      this.connections.self.gain * this.connections.self.weight * this.state +
+      this.bias;
+
+    // Activation sources coming from connections
+    for (let i = 0; i < this.connections.in.length; i++) {
+      const connection = this.connections.in[i];
+      this.state += connection.from.activation * connection.weight *
+        connection.gain;
+    }
+
+    // Squash the values received
+    this.activation = this.squash(this.state);
+    this.derivative = this.squash(this.state, true);
+
+    // Update traces
+    const nodes = [];
+    const influences = [];
+
+    for (let i = 0; i < this.connections.gated.length; i++) {
+      const conn = this.connections.gated[i];
+      const node = conn.to;
+
+      const index = nodes.indexOf(node);
+      if (index > -1) {
+        influences[index] += conn.weight * conn.from.activation;
+      } else {
+        nodes.push(node);
+        influences.push(
+          conn.weight * conn.from.activation +
+            (node.connections.self.gater === this ? node.old : 0),
+        );
+      }
+
+      // Adjust the gain to this nodes' activation
+      conn.gain = this.activation;
+    }
+
+    for (let i = 0; i < this.connections.in.length; i++) {
+      const connection = this.connections.in[i];
+
+      // Elegibility trace
+      connection.elegibility =
+        this.connections.self.gain * this.connections.self.weight *
+          connection.elegibility + connection.from.activation * connection.gain;
+
+      // Extended trace
+      for (let j = 0; j < nodes.length; j++) {
+        const node = nodes[j];
+        const influence = influences[j];
+
+        const index = connection.xtrace.nodes.indexOf(node);
+
+        if (index > -1) {
+          connection.xtrace.values[index] =
+            node.connections.self.gain * node.connections.self.weight *
+              connection.xtrace.values[index] +
+            this.derivative * connection.elegibility * influence;
+        } else {
+          // Does not exist there yet, might be through mutation
+          connection.xtrace.nodes.push(node);
+          connection.xtrace.values.push(
+            this.derivative * connection.elegibility * influence,
+          );
+        }
+      }
+    }
+
+    return this.activation;
+  },
+
+  /**
+   * Activates the node without calculating elegibility traces and such
+   */
+  noTraceActivate: function (input) {
+    // Check if an input is given
+    if (typeof input !== "undefined") {
+      this.activation = input;
+      return this.activation;
+    }
+
+    // All activation sources coming from the node itself
+    this.state =
+      this.connections.self.gain * this.connections.self.weight * this.state +
+      this.bias;
+
+    // Activation sources coming from connections
+
+    for (let i = this.connections.in.length; i--;) {
+      const connection = this.connections.in[i];
+      this.state += connection.from.activation * connection.weight *
+        connection.gain;
+    }
+
+    // Squash the values received
+    this.activation = this.squash(this.state);
+
+    for (let i = this.connections.gated.length; i--;) {
+      this.connections.gated[i].gain = this.activation;
+    }
+
+    return this.activation;
+  },
+
+  /**
+   * Back-propagate the error, aka learn
+   */
+  propagate: function (rate, momentum, update, target) {
+    momentum = momentum || 0;
+    rate = rate || 0.3;
+
+    // Error accumulator
+    let error = 0;
+
+    // Output nodes get their error from the enviroment
+    if (this.type === "output") {
+      this.error.responsibility = this.error.projected = target -
+        this.activation;
+    } else { // the rest of the nodes compute their error responsibilities by backpropagation
+      // error responsibilities from all the connections projected from this node
+
+      for (let i = 0; i < this.connections.out.length; i++) {
+        const connection = this.connections.out[i];
+        const node = connection.to;
+        // Eq. 21
+        error += node.error.responsibility * connection.weight *
+          connection.gain;
+      }
+
+      // Projected error responsibility
+      this.error.projected = this.derivative * error;
+
+      // Error responsibilities from all connections gated by this neuron
+      error = 0;
+
+      for (let i = 0; i < this.connections.gated.length; i++) {
+        const conn = this.connections.gated[i];
+        const node = conn.to;
+        let influence = node.connections.self.gater === this ? node.old : 0;
+
+        influence += conn.weight * conn.from.activation;
+        error += node.error.responsibility * influence;
+      }
+
+      // Gated error responsibility
+      this.error.gated = this.derivative * error;
+
+      // Error responsibility
+      this.error.responsibility = this.error.projected + this.error.gated;
+    }
+
+    if (this.type === "constant") return;
+
+    // Adjust all the node's incoming connections
+    for (let i = 0; i < this.connections.in.length; i++) {
+      const connection = this.connections.in[i];
+
+      let gradient = this.error.projected * connection.elegibility;
+
+      for (let j = 0; j < connection.xtrace.nodes.length; j++) {
+        const node = connection.xtrace.nodes[j];
+        const value = connection.xtrace.values[j];
+        gradient += node.error.responsibility * value;
+      }
+
+      // Adjust weight
+      const deltaWeight = rate * gradient;
+      connection.totalDeltaWeight += deltaWeight;
+      if (update) {
+        connection.totalDeltaWeight += momentum *
+          connection.previousDeltaWeight;
+        connection.weight += connection.totalDeltaWeight;
+        connection.previousDeltaWeight = connection.totalDeltaWeight;
+        connection.totalDeltaWeight = 0;
+      }
+    }
+
+    // Adjust bias
+    const deltaBias = rate * this.error.responsibility;
+    this.totalDeltaBias += deltaBias;
+    if (update) {
+      this.totalDeltaBias += momentum * this.previousDeltaBias;
+      this.bias += this.totalDeltaBias;
+      this.previousDeltaBias = this.totalDeltaBias;
+      this.totalDeltaBias = 0;
+    }
+  },
+
+  /**
+   * Creates a connection from this node to the given node
+   */
+  connect: function (target, weight) {
+    const connections = [];
+    if (typeof target.bias !== "undefined") { // must be a node!
+      if (target === this) {
+        // Turn on the self connection by setting the weight
+        if (this.connections.self.weight !== 0) {
+          console.warn("This connection already exists!");
+        } else {
+          this.connections.self.weight = weight || 1;
+        }
+        connections.push(this.connections.self);
+      } else if (this.isProjectingTo(target)) {
+        throw new Error("Already projecting a connection to this node!");
+      } else {
+        const connection = new Connection(this, target, weight);
+        target.connections.in.push(connection);
+        this.connections.out.push(connection);
+
+        connections.push(connection);
+      }
+    } else { // should be a group
+      for (let i = 0; i < target.nodes.length; i++) {
+        const connection = new Connection(this, target.nodes[i], weight);
+        target.nodes[i].connections.in.push(connection);
+        this.connections.out.push(connection);
+        target.connections.in.push(connection);
+
+        connections.push(connection);
+      }
+    }
+    return connections;
+  },
+
+  /**
+   * Disconnects this node from the other node
+   */
+  disconnect: function (node, twosided) {
+    if (this === node) {
+      this.connections.self.weight = 0;
+      return;
+    }
+
+    for (let i = 0; i < this.connections.out.length; i++) {
+      const conn = this.connections.out[i];
+      if (conn.to === node) {
+        this.connections.out.splice(i, 1);
+        const j = conn.to.connections.in.indexOf(conn);
+        conn.to.connections.in.splice(j, 1);
+        if (conn.gater !== null) conn.gater.ungate(conn);
+        break;
+      }
+    }
+
+    if (twosided) {
+      node.disconnect(this);
+    }
+  },
+
+  /**
+   * Make this node gate a connection
+   */
+  gate: function (connections) {
+    if (!Array.isArray(connections)) {
+      connections = [connections];
+    }
+
+    for (let i = 0; i < connections.length; i++) {
+      const connection = connections[i];
+
+      this.connections.gated.push(connection);
+      connection.gater = this;
+    }
+  },
+
+  /**
+   * Removes the gates from this node from the given connection(s)
+   */
+  ungate: function (connections) {
+    if (!Array.isArray(connections)) {
+      connections = [connections];
+    }
+
+    for (let i = connections.length - 1; i >= 0; i--) {
+      const connection = connections[i];
+
+      const index = this.connections.gated.indexOf(connection);
+      this.connections.gated.splice(index, 1);
+      connection.gater = null;
+      connection.gain = 1;
+    }
+  },
+
+  /**
+   * Clear the context of the node
+   */
+  clear: function () {
+    for (let i = 0; i < this.connections.in.length; i++) {
+      const connection = this.connections.in[i];
+
+      connection.elegibility = 0;
+      connection.xtrace = {
+        nodes: [],
+        values: [],
+      };
+    }
+
+    for (let i = 0; i < this.connections.gated.length; i++) {
+      const conn = this.connections.gated[i];
+      conn.gain = 0;
+    }
+
+    this.error.responsibility = this.error.projected = this.error.gated = 0;
+    this.old = this.state = this.activation = 0;
+  },
+
+  /**
+   * Mutates the node with the given method
+   */
+  mutate: function (method) {
+    if (typeof method === "undefined") {
+      throw new Error("No mutate method given!");
+    } /*else if (!(method.name in Mutation.ALL)) {
+      throw new Error("This method does not exist!");
+    }*/
+
+    switch (method) {
+      case Mutation.MOD_ACTIVATION: {
+        // Can't be the same squash
+        const squash = method
+          .allowed[
+            (method.allowed.indexOf(this.squash) +
+              Math.floor(Math.random() * (method.allowed.length - 1)) + 1) %
+            method.allowed.length
+          ];
+        this.squash = squash;
+        break;
+      }
+      case Mutation.MOD_BIAS: {
+        const modification = Math.random() * (method.max - method.min) +
+          method.min;
+        this.bias += modification;
+        break;
+      }
+    }
+  },
+
+  /**
+   * Checks if this node is projecting to the given node
+   */
+  isProjectingTo: function (node) {
+    if (node === this && this.connections.self.weight !== 0) return true;
+
+    for (let i = 0; i < this.connections.out.length; i++) {
+      const conn = this.connections.out[i];
+      if (conn.to === node) {
+        return true;
+      }
+    }
+    return false;
+  },
+
+  /**
+   * Checks if the given node is projecting to this node
+   */
+  isProjectedBy: function (node) {
+    if (node === this && this.connections.self.weight !== 0) return true;
+
+    for (let i = 0; i < this.connections.in.length; i++) {
+      const conn = this.connections.in[i];
+      if (conn.from === node) {
+        return true;
+      }
+    }
+
+    return false;
+  },
+
+  /**
+   * Converts the node to a json object
+   */
+  toJSON: function () {
+    const json = {
+      bias: this.bias,
+      type: this.type,
+      squash: this.squash.name,
+    };
+
+    return json;
+  },
+};
+
+/**
+ * Convert a json object to a node
+ */
+Node.fromJSON = function (json) {
+  const node = new Node();
+  node.bias = json.bias;
+  node.type = json.type;
+  node.squash = Methods.activation[json.squash];
+
+  return node;
+};
