1dc3453817e191b820d20e7fa94047ad51242143
Nigel (MacBook) <nigel@laptop>
2022-07-02 14:48:16 +1000

    temp

--- src/methods/activations/aggregate/IF.ts
+++ src/methods/activations/aggregate/IF.ts
@@ -9,22 +9,22 @@ export class IF implements NodeActivationInterface {
   }
 
   activate(node: Node) {
-    const conditions: number[] = [];
+    // const conditions: number[] = [];
 
-    // Activation sources coming from connections
-    for (let i = 0; i < node.connections.in.length; i++) {
-      const connection = node.connections.in[i];
-      values.push(
-        connection.from.getActivation() * connection.weight *
-          connection.gain,
-      );
-    }
+    // // Activation sources coming from connections
+    // for (let i = 0; i < node.connections.in.length; i++) {
+    //   const connection = node.connections.in[i];
+    //   values.push(
+    //     connection.from.getActivation() * connection.weight *
+    //       connection.gain,
+    //   );
+    // }
 
     // { from: 2, to: 3, type:"positive"},
     // { from: 1, to: 3, type:"condition" },
     // { from: 0, to: 3, type:"negative" },
 
     // const value = Math.hypot(...values);
-    return value;
+    return 0;
   }
 }
