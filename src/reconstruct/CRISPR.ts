a9decc9c62392a6ac81bb3233cd012256aef39d1
Nigel Leck <monster.flying.s@gmail.com>
2022-09-10 16:56:07 +1000

    Crisper (#16)
    * IF working
    
    * fixed volume.
    
    * format
    
    Co-authored-by: Nigel (MacBook) <nigel@laptop>

---
+++ src/reconstruct/CRISPR.ts
@@ -0,0 +1,104 @@
+import { NetworkInterface } from "../architecture/NetworkInterface.ts";
+import { Network } from "../architecture/network.js";
+import { Node } from "../architecture/Node.ts";
+import { addTag, getTag, TagsInterface } from "../tags/TagsInterface.ts";
+
+export interface CrisprInterface extends TagsInterface {
+  id: string;
+  mode: "append";
+
+  nodes: {
+    index: number;
+    type: "output";
+    squash: string;
+    bias: number;
+  }[];
+
+  connections: {
+    from?: number;
+    fromRelative?: number;
+    to?: number;
+    toRelative?: number;
+    weight: number;
+    type?: "positive" | "negative" | "condition";
+  }[];
+}
+
+export class CRISPR {
+  private network;
+
+  constructor(
+    network: NetworkInterface,
+  ) {
+    this.network = Network.fromJSON((network as Network).toJSON());
+  }
+
+  apply(dna: CrisprInterface): NetworkInterface {
+    const tmpNetwork = Network.fromJSON((this.network as Network).toJSON());
+
+    let alreadyProcessed = false;
+    tmpNetwork.nodes.forEach((node) => {
+      const id = getTag(node, "CRISPR");
+
+      if (id === dna.id) {
+        alreadyProcessed = true;
+      }
+    });
+
+    if (alreadyProcessed) return tmpNetwork;
+
+    let firstDnaOutputIndex = -1;
+    dna.nodes.forEach((node) => {
+      if (node.type == "output") {
+        if (firstDnaOutputIndex == -1) {
+          firstDnaOutputIndex = node.index;
+        }
+      }
+    });
+    let firstNetworkOutputIndex = -1;
+    tmpNetwork.nodes.forEach((node, indx) => {
+      if (node.type == "output") {
+        if (firstNetworkOutputIndex == -1) {
+          firstNetworkOutputIndex = indx;
+          node.type = "hidden";
+        }
+      }
+    });
+
+    const adjustIndx = firstNetworkOutputIndex - firstDnaOutputIndex +
+      dna.nodes.length;
+
+    dna.nodes.forEach((dnaNode) => {
+      const indx = dnaNode.index + adjustIndx;
+      const networkNode = new Node(
+        dnaNode.type,
+        dnaNode.bias,
+        tmpNetwork.util,
+        dnaNode.squash,
+      );
+      networkNode.index = indx;
+
+      addTag(networkNode, "CRISPR", dna.id);
+      tmpNetwork.nodes.push(networkNode);
+      if (dnaNode.type == "output") {
+        if (firstDnaOutputIndex == -1) {
+          firstDnaOutputIndex = indx;
+        }
+      }
+    });
+
+    dna.connections.forEach((c) => {
+      const from = c.from !== undefined
+        ? c.from
+        : ((c.fromRelative ? c.fromRelative : 0) + adjustIndx);
+      if (from == undefined) throw "invalid connection " + c;
+      const to = c.to !== undefined
+        ? c.to
+        : ((c.toRelative ? c.toRelative : 0) + adjustIndx);
+      if (to == undefined) throw "invalid connection " + c;
+      console.info(c);
+      tmpNetwork.util.connect(from, to, c.weight, c.type);
+    });
+    return tmpNetwork;
+  }
+}
---
+++ test/CRISPR.ts
@@ -0,0 +1,79 @@
+import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
+
+import { NetworkUtil } from "../src/architecture/NetworkUtil.ts";
+
+import { CRISPR } from "../src/reconstruct/CRISPR.ts";
+import { Network } from "../src/architecture/network.js";
+
+((globalThis as unknown) as { DEBUG: boolean }).DEBUG = true;
+
+Deno.test("CRISPR", () => {
+  const networkTXT = Deno.readTextFileSync("test/data/CRISPR/network.json");
+  const network = NetworkUtil.fromJSON(JSON.parse(networkTXT));
+
+  const crispr = new CRISPR(network);
+  const dnaTXT = Deno.readTextFileSync("test/data/CRISPR/DNA-IF.json");
+
+  const networkIF = crispr.apply(JSON.parse(dnaTXT));
+
+  const expectedJSON = JSON.parse(
+    Deno.readTextFileSync("test/data/CRISPR/expected-IF.json"),
+  );
+  const rawExpectedTXT = JSON.stringify(expectedJSON, null, 2);
+  const expectedTXT = JSON.stringify(
+    Network.fromJSON(JSON.parse(rawExpectedTXT)),
+    null,
+    2,
+  );
+  Deno.writeTextFileSync("test/data/CRISPR/expected-IF.json", expectedTXT);
+  const actualTXT = JSON.stringify((networkIF as Network).toJSON(), null, 2);
+
+  Deno.writeTextFileSync("test/data/CRISPR/.actual-IF.json", actualTXT);
+  assertEquals(actualTXT, expectedTXT, "should have converted");
+});
+
+Deno.test("CRISPR_twice", () => {
+  const networkTXT = Deno.readTextFileSync("test/data/CRISPR/network.json");
+  const network = NetworkUtil.fromJSON(JSON.parse(networkTXT));
+
+  const crispr = new CRISPR(network);
+  const dnaTXT = Deno.readTextFileSync("test/data/CRISPR/DNA-IF.json");
+
+  const networkIF1 = crispr.apply(JSON.parse(dnaTXT));
+  const crispr2 = new CRISPR(networkIF1);
+  const networkIF2 = crispr2.apply(JSON.parse(dnaTXT));
+
+  const expectedJSON = JSON.parse(
+    Deno.readTextFileSync("test/data/CRISPR/expected-IF.json"),
+  );
+  const expectedTXT = JSON.stringify(expectedJSON, null, 2);
+  const actualTXT = JSON.stringify((networkIF2 as Network).toJSON(), null, 2);
+
+  Deno.writeTextFileSync("test/data/CRISPR/.actual-IF.json", actualTXT);
+  assertEquals(actualTXT, expectedTXT, "should have converted");
+});
+
+Deno.test("CRISPR-Volume", () => {
+  const networkTXT = Deno.readTextFileSync("test/data/CRISPR/network.json");
+  const network = NetworkUtil.fromJSON(JSON.parse(networkTXT));
+
+  const crispr = new CRISPR(network);
+  const dnaTXT = Deno.readTextFileSync("test/data/CRISPR/DNA-VOLUME.json");
+
+  const networkIF = crispr.apply(JSON.parse(dnaTXT));
+
+  const expectedJSON = JSON.parse(
+    Deno.readTextFileSync("test/data/CRISPR/expected-VOLUME.json"),
+  );
+  const rawExpectedTXT = JSON.stringify(expectedJSON, null, 2);
+  const expectedTXT = JSON.stringify(
+    Network.fromJSON(JSON.parse(rawExpectedTXT)),
+    null,
+    2,
+  );
+  Deno.writeTextFileSync("test/data/CRISPR/expected-VOLUME.json", expectedTXT);
+  const actualTXT = JSON.stringify((networkIF as Network).toJSON(), null, 2);
+
+  Deno.writeTextFileSync("test/data/CRISPR/.actual-VOLUME.json", actualTXT);
+  assertEquals(actualTXT, expectedTXT, "should have converted");
+});
