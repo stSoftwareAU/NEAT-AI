86b8d1fd98587f7e0f8fb37ab5f53593ce8ce670
Nigel Leck <monster.flying.s@gmail.com>
2022-09-24 17:39:13 +1000

    Keep crispr (#26)
    * Cross over mandates tags.
    
    * Increase error if completely wrong
    
    Co-authored-by: Nigel (MacBook) <nigel@laptop>

--- test/Costs.ts
+++ test/Costs.ts
@@ -1,4 +1,6 @@
+import { assert, assertAlmostEquals } from "https://deno.land/std@0.157.0/testing/asserts.ts";
 import { Costs } from "../src/Costs.ts";
+import { MSELimit } from "../src/costs/MSELimit.ts";
 
 ((globalThis as unknown) as { DEBUG: boolean }).DEBUG = true;
 
@@ -33,5 +35,38 @@ Deno.test("calculate cost", () => {
 
       console.info(name, check, r);
     }
+  });
+});
+
+
+Deno.test("MSELimit", () => {
+  const cost=new MSELimit();
+
+  const exact=[-1,-0.5, 0, 0.5, 1];
+
+  exact.forEach( v =>{
+    const c=cost.calculate([v],[v]);
+
+    assertAlmostEquals( c, 0, 0.000001, "Should be near zero for: " + v + " was: " + c);
+  });
+
+  const checks=[
+    {
+      target: [1],
+      output: [2],
+      error: 0
+    },
+    {
+      target: [-0.51],
+      output: [0.9],
+      error: 3.9762
+    }
+  ];
+
+  checks.forEach( check=> {
+    const error=cost.calculate(check.target,check.output);
+
+
+    assertAlmostEquals(error, check.error, 0.000001, "Should be near zero for: " + check + " was: " + error);
   });
 });
