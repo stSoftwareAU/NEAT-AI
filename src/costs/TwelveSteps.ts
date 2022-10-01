d086e968bb64f5c0d082f0ab9b02fdc70f7e4c62
Nigel Leck <monster.flying.s@gmail.com>
2022-10-01 14:42:33 +1000

    added twelve steps (#28)
    Co-authored-by: Nigel (MacBook) <nigel@laptop>

---
+++ src/costs/TwelveSteps.ts
@@ -0,0 +1,28 @@
+import { CostInterface } from "../Costs.ts";
+
+/** Twelve steps Error */
+export class TwelveSteps implements CostInterface {
+  calculate(target: number[], output: number[]): number {
+    let error = 0;
+    const len = output.length;
+
+    for (let i = len; i--;) {
+      const t = target[i];
+
+      const t1 = Math.round(Math.min(1.1, Math.max(-1.1, t)) * 10);
+
+      const o = output[i];
+      const o1 = Math.round(Math.min(1.1, Math.max(-1.1, o)) * 10);
+
+      if (t1 >= 10 && o1 < -10 || t1 <= -10 && o1 > 10) {
+        error += 1000;
+      } else if (t1 >= 5 && o1 <= -5 || t1 <= -5 && o1 >= 5) {
+        error += Math.pow(t1 - o1, 2) * 2;
+      } else {
+        error += Math.pow(t1 - o1, 2);
+      }
+    }
+
+    return error / len;
+  }
+}
