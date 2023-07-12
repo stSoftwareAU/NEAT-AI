ebb47c3e94bbac8c37d18766e216a2e023851053
Nigel Leck <monster.flying.s@gmail.com>
2023-07-13 09:06:38 +1000

    Upgraded standard library. (#111)
    Co-authored-by: Nigel Leck <nigel@stSoftware.com.au>

--- test/addNode.ts
+++ test/addNode.ts
@@ -1,4 +1,4 @@
-import { assert } from "https://deno.land/std@0.177.0/testing/asserts.ts";
+import { assert } from "https://deno.land/std@0.194.0/testing/asserts.ts";
 import { Network } from "../src/architecture/Network.ts";
 import { NetworkInternal } from "../src/architecture/NetworkInterfaces.ts";
 
