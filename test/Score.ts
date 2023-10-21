e7a1488038764ba65b8760de9c6f7d0249177b8f
Nigel Leck <nigel@stSoftware.com.au>
2023-10-21 15:49:33 +1100

    Clean up the tests

--- test/Score.ts
+++ test/Score.ts
@@ -1,69 +1,51 @@
 import {
-  assert,
-  assertAlmostEquals,
-} from "https://deno.land/std@0.204.0/assert/mod.ts";
-import { Network } from "../src/architecture/Network.ts";
-import { calculate, valuePenalty } from "../src/architecture/Score.ts";
-
-Deno.test("Score", () => {
-  const creature = Network.fromJSON(
-    JSON.parse(Deno.readTextFileSync("test/data/large.json")),
-  );
-
-  const scoreA = calculate(creature, 0.603, 0.000_000_1);
-
-  assertAlmostEquals(scoreA, -0.603_132, 0.000_001);
-
-  creature.connections.forEach((c) => {
-    if (c.from == 636 && c.to == 860) {
-      console.info(c);
-      c.weight /= 2;
-    }
+    assert,
+    assertAlmostEquals,
+  } from "https://deno.land/std@0.204.0/assert/mod.ts";
+  import { Network } from "../src/architecture/Network.ts";
+  import { calculate, valuePenalty } from "../src/architecture/Score.ts";
+  
+  function setupCreature() {
+    return Network.fromJSON(
+      JSON.parse(Deno.readTextFileSync("test/data/large.json")),
+    );
+  }
+  
+  Deno.test("Score: Calculation with given parameters", () => {
+    const creature = setupCreature();
+    const score = calculate(creature, 0.603, 0.000_000_1);
+    assertAlmostEquals(score, -0.603_132, 0.000_001);
   });
-
-  const scoreB = calculate(creature, 0.603, 0.000_000_1);
-
-  assert(scoreB > scoreA, `${scoreB} should be greater than ${scoreA}`);
-
-  let biggestBias = 0;
-  creature.nodes.forEach((n) => {
-    if (n.bias != Infinity) {
-      const b = Math.abs(n.bias);
-      if (b > biggestBias) {
-        biggestBias = b;
-        console.info(n.uuid, n.bias);
+  
+  Deno.test("Score: Weight change should affect score", () => {
+    const creature = setupCreature();
+    const initialScore = calculate(creature, 0.603, 0.000_000_1);
+  
+    // Modify weight of a specific connection
+    creature.connections.forEach((c) => {
+      if (c.from === 636 && c.to === 860) {
+        c.weight /= 2;
       }
-    }
+    });
+  
+    const newScore = calculate(creature, 0.603, 0.000_000_1);
+    assert(newScore > initialScore, `${newScore} should be greater than ${initialScore}`);
   });
-
-  creature.connections.forEach((c) => {
-    if (c.from == 636 && c.to == 860) {
-      console.info(c);
-      c.weight = 1;
-    }
+  
+  // Add more separate test cases here for different parts of the functionality
+  
+  Deno.test("valuePenalty: Edge Cases", () => {
+    assertAlmostEquals(valuePenalty(0), 0);
+    assertAlmostEquals(valuePenalty(1), 0);
+    assertAlmostEquals(valuePenalty(-1), 0);
+    assertAlmostEquals(valuePenalty(0.5), 0);
   });
-  const scoreC = calculate(creature, 0.603, 0.000_000_1);
-
-  creature.nodes.forEach((n) => {
-    if (n.uuid == "d049ceac-84d6-4c59-a057-630eefbc03d1") {
-      const b = n.bias;
-      n.bias = b / 2;
-    }
+  
+  Deno.test("valuePenalty: Various Values", () => {
+    assertAlmostEquals(valuePenalty(5000), 1.895, 0.001);
+    assertAlmostEquals(valuePenalty(1e10), 1.958, 0.001);
+    assertAlmostEquals(valuePenalty(1e100), 1.996, 0.001);
+    assertAlmostEquals(valuePenalty(1e200), 1.998, 0.001);
+    assertAlmostEquals(valuePenalty(184323183.02923888), 1.95, 0.001);
   });
-
-  const scoreD = calculate(creature, 0.603, 0.000_000_1);
-  assert(scoreD > scoreC, `${scoreD} should be greater than ${scoreC}`);
-});
-
-Deno.test("valuePenalty", () => {
-  assertAlmostEquals(valuePenalty(0), 0);
-  assertAlmostEquals(valuePenalty(1), 0);
-  assertAlmostEquals(valuePenalty(-1), 0);
-  assertAlmostEquals(valuePenalty(0.5), 0);
-
-  assertAlmostEquals(valuePenalty(5000), 1.895, 0.001);
-  assertAlmostEquals(valuePenalty(1e10), 1.958, 0.001);
-  assertAlmostEquals(valuePenalty(1e100), 1.996, 0.001);
-  assertAlmostEquals(valuePenalty(1e200), 1.998, 0.001);
-  assertAlmostEquals(valuePenalty(184323183.02923888), 1.95, 0.001);
-});
+  
