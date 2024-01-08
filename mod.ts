32fdb9900d2d1f15e2a583251c3ea054ddf733e5
Nigel Leck <monster.flying.s@gmail.com>
2024-01-09 10:49:04 +1100

    Add module (#185)
    * Fix network propagation bug and improve test coverage
    
    * Update quality.sh script to include mod.ts in deno fmt and deno lint commands
    
    * Add AI helper module
    
    ---------
    
    Co-authored-by: Nigel (MacBook) <nigel@laptop>

---
+++ mod.ts
@@ -0,0 +1,10 @@
+/**
+ * Helpers for working with the AI
+ *
+ * @module
+ */
+
+export * from "./src/Neat.ts";
+export * from "./src/Costs.ts";
+export * from "./src/config/NeatOptions.ts";
+export * from "./src/architecture/Network.ts";
--- quality.sh
+++ quality.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-deno fmt src test
-deno lint src test
+deno fmt src test mod.ts
+deno lint src test mod.ts
 rm -rf .trace
 deno test --allow-all --trace-ops --v8-flags=--max-old-space-size=8192 --parallel --config ./test/deno.json
