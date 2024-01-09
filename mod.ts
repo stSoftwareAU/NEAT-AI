f00cbb8fdf61b9fcf59761f5adcde9a36ba41d12
Nigel Leck <monster.flying.s@gmail.com>
2024-01-09 13:27:35 +1100

    Add NetworkInterfaces export (#188)
    Co-authored-by: Nigel (MacBook) <nigel@laptop>

--- mod.ts
+++ mod.ts
@@ -5,9 +5,9 @@
  */
 
 export { Network } from "./src/architecture/Network.ts";
+export type { NetworkExport, NetworkInternal, NetworkTrace } from "./src/architecture/NetworkInterfaces.ts";
 export { NetworkUtil } from "./src/architecture/NetworkUtils.ts";
 export type { NeatOptions } from "./src/config/NeatOptions.ts";
-
 export { Selection } from "./src/methods/Selection.ts";
 export { Mutation } from "./src/methods/mutation.ts";
 export { CRISPR } from "./src/reconstruct/CRISPR.ts";
