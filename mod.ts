5c9afc4a32dd2d17d79bbeb0cf791d7bae81d68b
Nigel Leck <monster.flying.s@gmail.com>
2024-01-09 11:08:11 +1100

    Remove unused exports and add new exports (#186)
    Co-authored-by: Nigel (MacBook) <nigel@laptop>

--- mod.ts
+++ mod.ts
@@ -4,7 +4,19 @@
  * @module
  */
 
-export * from "./src/Neat.ts";
-export * from "./src/Costs.ts";
-export * from "./src/config/NeatOptions.ts";
+// export * from "./src/Neat.ts";
+// export * from "./src/Costs.ts";
+// export * from "./src/config/NeatOptions.ts";
 export * from "./src/architecture/Network.ts";
+export type { NeatOptions } from "./src/config/NeatOptions.ts";
+export { NetworkUtil } from "./src/architecture/NetworkUtils.ts";
+
+export {CRISPR} from "./src/reconstruct/CRISPR.ts";
+export {Selection} from "./src/methods/Selection.ts";
+export {Mutation} from "./src/methods/mutation.ts";
+
+export {
+    addTag,
+    getTag,
+    removeTag,
+  } from "./src/tags/TagsInterface.ts";
