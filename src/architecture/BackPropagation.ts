d9f40bfa83ad08fce81bad9f88e5d24ac8bd0562
Nigel (MacBook) <nigel@laptop>
2024-01-14 14:53:29 +1100

    Remove commented out code in BackPropagation.ts

--- src/architecture/BackPropagation.ts
+++ src/architecture/BackPropagation.ts
@@ -193,7 +193,6 @@ export function accumulateWeight(
 }
 
 export function adjustedWeight(
-  // cs: ConnectionState,
   networkState: CreatureState,
   c: ConnectionInternal,
   config: BackPropagationConfig,
