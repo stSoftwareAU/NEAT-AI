9a467b0110fd39f4f0de3fed4fa5d5e70ca14a32
Nigel Leck <nigel@stSoftware.com.au>
2024-01-20 14:10:56 +1100

    Refactor Connection class constructor and toJSON method
    This commit refactors the Connection class constructor and toJSON method by removing unnecessary type checks and simplifying the code.

--- src/architecture/Connection.ts
+++ src/architecture/Connection.ts
@@ -19,15 +19,6 @@ export class Connection implements ConnectionInternal {
     weight: number,
     type?: "positive" | "negative" | "condition",
   ) {
-    if (Number.isInteger(from) == false || from < 0) {
-      throw new Error(`from should be a non-negative integer was: ${from}`);
-    }
-    if (Number.isInteger(to) == false || to < 0) {
-      throw new Error(`to should be a non-negative integer was: ${to}`);
-    }
-    if (!Number.isFinite(weight)) {
-      throw new Error(`weight not a number was: ${weight}`);
-    }
     this.from = from;
     this.to = to;
     this.type = type;
@@ -44,7 +35,7 @@ export class Connection implements ConnectionInternal {
       weight: this.weight,
       fromUUID: fromUUID ? fromUUID : `error-${this.from}`,
       toUUID: toUUID ? toUUID : `error-${this.to}`,
-      type: this.type ? this.type : undefined,
+      type: this.type,
     };
 
     return json;
@@ -55,7 +46,7 @@ export class Connection implements ConnectionInternal {
       weight: this.weight,
       from: this.from,
       to: this.to,
-      type: this.type ? this.type : undefined,
+      type: this.type,
     };
 
     return json;
