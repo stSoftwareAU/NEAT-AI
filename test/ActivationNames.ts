2f5f2c9f818e58f20411965418db0bdb35849088
Nigel Leck <monster.flying.s@gmail.com>
2024-03-08 21:16:33 +1100

    Update quality.sh and Creature.ts files (#308)
    Co-authored-by: Nigel Leck <nigel@stSoftware.com.au>

--- test/ActivationNames.ts
+++ test/ActivationNames.ts
@@ -1,20 +1,15 @@
-import { fail } from "https://deno.land/std@0.218.0/assert/fail.ts";
 import { Activations } from "../src/methods/activations/Activations.ts";
+import { assertNotEquals } from "https://deno.land/std@0.218.0/assert/assert_not_equals.ts";
+import { assert } from "https://deno.land/std@0.218.0/assert/assert.ts";
 
 Deno.test("ActivationNames", () => {
   Activations.NAMES.forEach((name) => {
     const activation = Activations.find(name);
-    if (!activation) {
-      throw new Error(`Could not find activation for ${name}`);
-    }
 
-    if (name == "INVERSE") {
-      fail("INVERSE is not a valid activation");
-    }
+    assert(activation !== undefined, `Could not find activation for ${name}`);
+    assertNotEquals(name, "INVERSE", "INVERSE is not a valid activation");
   });
 
   const inverse = Activations.find("INVERSE");
-  if (!inverse) {
-    throw new Error(`Could not find activation for INVERSE`);
-  }
+  assert(inverse !== undefined, `Could not find activation for INVERSE`);
 });
