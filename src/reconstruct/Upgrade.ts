0de84d12f047caf83042c199a7a24f932d5dbe6a
Nigel Leck <nigel@stSoftware.com.au>
2024-04-21 08:48:48 +1000

    Add CRISPR functionality to Upgrade class

--- src/reconstruct/Upgrade.ts
+++ src/reconstruct/Upgrade.ts
@@ -2,6 +2,7 @@ import { assert } from "https://deno.land/std@0.223.0/assert/mod.ts";
 import { CreatureExport } from "../architecture/CreatureInterfaces.ts";
 import { Creature } from "../Creature.ts";
 import { creatureValidate } from "../architecture/CreatureValidate.ts";
+import { CrisprInterface } from "./CRISPR.ts";
 
 export class Upgrade {
   static correct(
@@ -21,5 +22,27 @@ export class Upgrade {
     creature.fix();
     creatureValidate(creature);
     return creature;
+  }
+
+  static CRISPR(dnaLegacy: CrisprInterface): CrisprInterface {
+    const dnaClean: CrisprInterface = JSON.parse(JSON.stringify(dnaLegacy));
+    /* Legacy */
+    if ((dnaClean as unknown as { nodes: { squash: string }[] }).nodes) {
+      (dnaClean as unknown as { neurons: { squash: string }[] }).neurons =
+        (dnaClean as unknown as { nodes: { squash: string }[] }).nodes;
+    }
+
+    if (
+      (dnaClean as unknown as { connections: { weight: number }[] }).connections
+    ) {
+      (dnaClean as unknown as { synapses: { weight: number }[] }).synapses =
+        (dnaClean as unknown as { connections: { weight: number }[] })
+          .connections;
+    }
+
+    /* End Version 1 */
+    if (dnaClean.mode !== "insert") dnaClean.mode = "append";
+
+    return dnaClean;
   }
 }
---
+++ test/CRISPR/Aliases.ts
@@ -0,0 +1,26 @@
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.223.0/assert/mod.ts";
+import { CRISPR, CrisprInterface } from "../../src/reconstruct/CRISPR.ts";
+
+Deno.test("editAliases", () => {
+  const dna: CrisprInterface = {
+    id: "edit test",
+    mode: "insert",
+    synapses: [{
+      fromUUID: "abc",
+      toUUID: "xyz",
+      weight: 1,
+    }],
+  };
+  const aliases: Record<string, string> = {
+    "abc": "input-1",
+  };
+
+  const result = CRISPR.editAliases(dna, aliases);
+
+  assert(result.synapses);
+  const synapse = result.synapses[0];
+  assertEquals(synapse.fromUUID, "input-1");
+});
