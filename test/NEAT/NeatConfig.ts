5f59ac56ec88209f24e0830ee9fe9a97c5a94da1
Nigel Leck <nigel@stSoftware.com.au>
2024-06-09 15:26:23 +1000

    feat: Add unit tests for NeatConfig class and update config values
    This commit adds unit tests for the `NeatConfig` class to ensure that the `debug`, `mutationAmount`, `selection`, and `verbose` properties are correctly set when creating a new instance of `NeatConfig` with different values.
    
    Co-authored-by: Nigel Leck <nigel@stSoftware.com.au>

--- test/NEAT/NeatConfig.ts
+++ test/NEAT/NeatConfig.ts
@@ -1,9 +1,26 @@
 import { assertEquals } from "@std/assert";
 import { NeatConfig } from "../../src/config/NeatConfig.ts";
+import { Selection } from "../../mod.ts";
 
 Deno.test("NeatConfig debug", () => {
     const config = new NeatConfig({debug:true});
     assertEquals(config.debug, true);
     const config2 = new NeatConfig({debug:false});
     assertEquals(config2.debug, false);
+});
+
+Deno.test("NeatConfig mutationAmount", () => {
+    const config = new NeatConfig({mutationAmount:-2});
+    assertEquals(config.mutationAmount, 1);
+});
+
+Deno.test("NeatConfig selection", () => {
+    const config = new NeatConfig({selection:Selection.FITNESS_PROPORTIONATE});
+    assertEquals(config.selection, Selection.FITNESS_PROPORTIONATE);
+});
+
+
+Deno.test("NeatConfig verbose", () => {
+    const config = new NeatConfig({verbose:true});
+    assertEquals(config.verbose, true);
 });
