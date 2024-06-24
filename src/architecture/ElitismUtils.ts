4ed1efd9fe7a5335f0709666d0edbff8d517d53e
Nigel Leck <nigel@stSoftware.com.au>
2024-06-24 11:01:36 +1000

    Refactor checkForRecursiveSynapse function to handle visited neurons correctly

--- src/architecture/ElitismUtils.ts
+++ src/architecture/ElitismUtils.ts
@@ -2,6 +2,7 @@ import { blue, bold, green, red, white, yellow } from "@std/fmt/colors";
 import { addTag, getTag } from "@stsoftware/tags";
 import type { Creature } from "../Creature.ts";
 import type { Approach } from "../NEAT/LogApproach.ts";
+import { assert } from "@std/assert";
 
 interface ElitistsResults {
   elitists: Creature[];
@@ -51,7 +52,8 @@ function logVerbose(creatures: Creature[]): number {
   for (let indx = 0; indx < creatures.length; indx++) {
     const creature = creatures[indx];
     const score = creature.score;
-    totalScore += score ? score : 0;
+    assert(score !== undefined, "Creature must have a score");
+    totalScore += score;
 
     const notified = getTag(creature, "notified");
     if (notified === "Yes") {
@@ -62,16 +64,14 @@ function logVerbose(creatures: Creature[]): number {
       addTag(creature, "notified", "Yes");
 
       const approach = getTag(creature, "approach") as Approach;
-      const untrainedError = getTag(creature, "untrained-error");
-      const error = getTag(creature, "error");
-      const diff = Number.parseFloat(untrainedError ?? "99999") -
-        Number.parseFloat(error ?? "99999");
+      const untrainedError = getTag(creature, "untrained-error") ?? "99999";
+      const error = getTag(creature, "error") ?? "99999";
+      const diff = Number.parseFloat(untrainedError) -
+        Number.parseFloat(error);
       console.info(
         `${approach} ${blue(trainID)} Score: ${
-          yellow(score ? score.toString() : "undefined")
-        }, Error: ${yellow(untrainedError ?? "unknown")} -> ${
-          yellow(error ?? "unknown")
-        }` + (diff > 0
+          yellow(score.toString())
+        }, Error: ${yellow(untrainedError)} -> ${yellow(error)}` + (diff > 0
           ? ` ${"improved " + bold(green(diff.toString()))}`
           : diff < 0
           ? ` ${"regression " + red(diff.toString())}`
@@ -85,18 +85,17 @@ function logVerbose(creatures: Creature[]): number {
 
       if (sourceCreature) {
         const score = creature.score;
-        const sourceError = getTag(sourceCreature, "error");
-        const error = getTag(creature, "error");
-        const diff = Number.parseFloat(sourceError ?? "99999") -
-          Number.parseFloat(error ?? "99999");
+        assert(score !== undefined, "Creature must have a score");
+        const sourceError = getTag(sourceCreature, "error") ?? "99999";
+        const error = getTag(creature, "error") ?? "99999";
+        const diff = Number.parseFloat(sourceError) -
+          Number.parseFloat(error);
         const dnaID = getTag(creature, "CRISPR-DNA");
 
         console.info(
           `CRISPR ${blue(dnaID ?? "unknown")} Score: ${
-            yellow(score ? score.toString() : "undefined")
-          }, Error: ${yellow(sourceError ?? "unknown")} -> ${
-            yellow(error ?? "unknown")
-          }` + (diff > 0
+            yellow(score.toString())
+          }, Error: ${yellow(sourceError)} -> ${yellow(error)}` + (diff > 0
             ? ` ${"improved " + bold(green(diff.toString()))}`
             : diff < 0
             ? ` ${"regression " + red(diff.toString())}`
