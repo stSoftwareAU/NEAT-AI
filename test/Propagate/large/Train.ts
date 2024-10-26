61a4a8d86e80b2412a583d665fac48acd904c911
Nigel Leck <nigel@stSoftware.com.au>
2024-10-26 18:54:50 +1100

    Refactor SINUSOID unSquash method in src/methods/activations/types/SINUSOID.ts

--- test/Propagate/large/Train.ts
+++ test/Propagate/large/Train.ts
@@ -4,7 +4,6 @@ import { Costs } from "../../../src/Costs.ts";
 import { Creature } from "../../../src/Creature.ts";
 import { train } from "../../propagate/TrainTestOnlyUtil.ts";
 
-
 ((globalThis as unknown) as { DEBUG: boolean }).DEBUG = true;
 
 Deno.test("large", () => {
