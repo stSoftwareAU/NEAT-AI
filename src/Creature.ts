5516d6d3173a89f0823ddf174ca8870393ba6487
Nigel Leck <nigel@stSoftware.com.au>
2024-12-16 12:57:17 +1100

    fix: update state handling in Creature class to set preparedNeurons to false

--- src/Creature.ts
+++ src/Creature.ts
@@ -704,7 +704,7 @@ export class Creature implements CreatureInternal {
     if (changed) {
       delete this.uuid;
       delete this.memetic;
-      this.state.clear();
+      this.state.preparedNeurons = false;
       this.fix();
     }
 
@@ -1174,7 +1174,7 @@ export class Creature implements CreatureInternal {
 
     if (changed) {
       delete this.uuid;
-      this.state.clear();
+      this.state.preparedNeurons = false;
       this.fix();
     }
     if (this.DEBUG) {
