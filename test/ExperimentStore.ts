bfd3d2cb15a86332041c281a619cf796a77eec93
Nigel Leck <nigel@stSoftware.com.au>
2025-05-03 17:41:43 +1000

    bump version to 0.169.4; reorder imports in ExperimentStore test file

--- test/ExperimentStore.ts
+++ test/ExperimentStore.ts
@@ -1,12 +1,12 @@
 import { assert } from "@std/assert";
 import { addTag } from "@stsoftware/tags";
-import { Creature } from "../src/Creature.ts";
 import { CreatureUtil } from "../src/architecture/CreatureUtils.ts";
-import { Neat } from "../src/NEAT/Neat.ts";
 import { DeDuplicator } from "../src/architecture/DeDuplicator.ts";
-import { Mutator } from "../src/NEAT/Mutator.ts";
 import { Breed } from "../src/breed/Breed.ts";
+import { Creature } from "../src/Creature.ts";
 import { Genus } from "../src/NEAT/Genus.ts";
+import { Mutator } from "../src/NEAT/Mutator.ts";
+import { Neat } from "../src/NEAT/Neat.ts";
 
 ((globalThis as unknown) as { DEBUG: boolean }).DEBUG = true;
 
@@ -69,11 +69,6 @@ function previousExperiment(creature: Creature, neat: Neat) {
   const mutator = new Mutator(neat.config);
   const genus = new Genus();
 
-  // The population is already sorted in the desired order
-  for (let i = 0; i < neat.population.length; i++) {
-    const creature = neat.population[i];
-    genus.addCreature(creature);
-  }
   const breed = new Breed(genus, neat.config);
   const deDuplicator = new DeDuplicator(breed, mutator);
 
