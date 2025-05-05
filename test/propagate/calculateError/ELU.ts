71553a097b17f825a60c0b1372b15ff24f1072ff
Nigel (MacBook) <nigel@laptop>
2025-05-05 21:55:45 +1000

    add calculateError method for ELU activation function with tests

---
+++ test/propagate/calculateError/ELU.ts
@@ -0,0 +1,28 @@
+import { assertEquals, assert } from "@std/assert";
+import { ELU } from "../../../src/methods/activations/types/ELU.ts";
+
+Deno.test("ELU.calculateError: positive input", () => {
+  const elu = new ELU();
+
+  assertEquals(elu.calculateError(1.0, 1.0, 1.0), 0);
+  const e = elu.calculateError(0.8, 1.2, 1.0);
+  assert(Number.isFinite(e));
+  assert(e > 0);
+});
+
+Deno.test("ELU.calculateError: negative input", () => {
+  const elu = new ELU();
+
+  const act = elu.squash(-1.0);
+  const e = elu.calculateError(act, 0.5, -1.0);
+  assert(Number.isFinite(e));
+  assert(e > 0);
+});
+
+Deno.test("ELU.calculateError: zero input", () => {
+  const elu = new ELU();
+
+  const act = elu.squash(0.0);
+  const e = elu.calculateError(act, 0.2, 0.0);
+  assert(Number.isFinite(e));
+});
