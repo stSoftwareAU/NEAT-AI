ba012fcccf96b864e472010380f55cf0110bb1c3
Nigel (MacBook) <nigel@laptop>
2025-05-20 06:45:07 +1000

    bump version to 0.177.7 and update complexityPenalty in HYPOT and HYPOTv2 classes

--- src/methods/activations/aggregate/HYPOT.ts
+++ src/methods/activations/aggregate/HYPOT.ts
@@ -13,7 +13,7 @@ import { IDENTITY } from "../types/IDENTITY.ts";
 export class HYPOT
   implements NeuronActivationInterface, MakeActivationFunctionInterface {
   public static NAME = "HYPOT";
-  complexityPenalty = 10;
+  complexityPenalty = 100;
   public readonly range = new ActivationRange(
     HYPOT.NAME,
     Number.MIN_SAFE_INTEGER,
